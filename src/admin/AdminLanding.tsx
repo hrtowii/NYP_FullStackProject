23460d93a3901b204ea04a42a6da06a2b73f32f0
hrtowii <68852354+hrtowii@users.noreply.github.com>
2024-08-01 17:05:54 +0800

    admin side

--- src/admin/AdminLanding.tsx
+++ src/admin/AdminLanding.tsx
@@ -24,11 +24,12 @@ const getUserRole = (user) => {
 
 export default function AdminLanding() {
   const [openModal, setOpenModal] = useState(false);
-  const [editData, setEditData] = useState({ id: '', name: '', email: '' });
+  const [editData, setEditData] = useState({});
   const { token } = useContext(TokenContext);
   const [users, setUsers] = useState([]);
   const [events, setEvents] = useState([]);
-  const [reviews, setReviews] = useState([]);
+  const [donations, setDonations] = useState([]);
+  const [reservations, setReservations] = useState([]);
   const [snackbarOpen, setSnackbarOpen] = useState(false);
   const [activeTab, setActiveTab] = useState(0);
 
@@ -42,26 +43,52 @@ export default function AdminLanding() {
 
   const eventColumns = [
     { id: 'id', label: 'ID', minWidth: 50 },
-    { id: 'name', label: 'Name', minWidth: 150 },
+    { id: 'name', label: 'Name', minWidth: 75 },
     { id: 'date', label: 'Date', minWidth: 100 },
-    { id: 'location', label: 'Location', minWidth: 150 },
+    { id: 'location', label: 'Location', minWidth: 100 },
     { id: 'actions', label: 'Actions', minWidth: 100 },
   ];
 
-  const reviewColumns = [
+  const donationColumns = [
     { id: 'id', label: 'ID', minWidth: 50 },
-    { id: 'rating', label: 'Rating', minWidth: 50 },
-    { id: 'userId', label: 'User ID', minWidth: 100 },
-    { id: 'comment', label: 'Comment', minWidth: 200 },
-    { id: 'createdAt', label: 'Created At', minWidth: 150 },
-    { id: 'updatedAt', label: 'Updated At', minWidth: 150 },
+    { id: 'category', label: 'Category', minWidth: 75 },
+    { id: 'donatorId', label: 'Donator ID', minWidth: 75 },
+    { id: 'location', label: 'Location', minWidth: 100 },
+    { id: 'createdAt', label: 'Created At', minWidth: 100, format: (value) => {
+      const date = new Date(value);
+      return `${date.getDate().toString().padStart(2, '0')}/${(date.getMonth() + 1).toString().padStart(2, '0')} - ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`;
+    } },
+    { id: 'remarks', label: 'Remarks', minWidth: 50 },
     { id: 'actions', label: 'Actions', minWidth: 100 },
   ];
+
+  const reservationColumns = [
+    { id: 'id', label: 'ID', minWidth: 50 },
+    { id: 'userId', label: 'User ID', minWidth: 75 },
+    { id: 'eventId', label: 'Event ID', minWidth: 75 },
+    { id: 'status', label: 'Status', minWidth: 100 },
+    { id: 'actions', label: 'Actions', minWidth: 100 },
+  ];
+
+  const donationNestedConfig = {
+    key: 'foods',
+    label: 'Foods',
+    columns: [
+      { id: 'name', label: 'Name', minWidth: 100 },
+      { id: 'quantity', label: 'Quantity', minWidth: 50 },
+      { id: 'type', label: 'Type', minWidth: 100 },
+      { id: 'expiryDate', label: 'Expiry Date', minWidth: 100, format: (value) => {
+        const date = new Date(value);
+        return `${date.getDate().toString().padStart(2, '0')}/${(date.getMonth() + 1).toString().padStart(2, '0')} - ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`;
+      } },
+    ],
+  }
 
   useEffect(() => {
     fetchUsers();
     fetchEvents();
-    fetchReviews();
+    fetchDonations();
+    fetchReservations();
   }, [token]);
 
   const fetchUsers = async () => {
@@ -83,7 +110,7 @@ export default function AdminLanding() {
 
   const fetchEvents = async () => {
     try {
-      const response = await fetch(`${backendRoute}/events`, {
+      const response = await fetch(`${backendRoute}/donator/events`, {
         method: 'GET',
         headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` },
       });
@@ -95,19 +122,34 @@ export default function AdminLanding() {
     }
   };
 
-  const fetchReviews = async () => {
+  const fetchDonations = async () => {
     try {
-      const response = await fetch(`${backendRoute}/reviews`, {
+      const response = await fetch(`${backendRoute}/donations`, {
         method: 'GET',
         headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` },
       });
       const data = await response.json();
       console.log(data)
-      setReviews(data);
+      setDonations(data.donations);
     } catch (error) {
-      console.error('Error fetching events:', error);
+      console.error('Error fetching donations:', error);
     }
   }
+
+  const fetchReservations = async () => {
+    try {
+      const response = await fetch(`${backendRoute}/reservations`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` },
+      });
+      const data = await response.json();
+      console.log(data)
+      setReservations(data);
+    } catch (error) {
+      console.error('Error fetching reservations:', error);
+    }
+  }
+
   const handleOpenModal = (row) => {
     setEditData(row);
     setOpenModal(true);
@@ -123,14 +165,43 @@ export default function AdminLanding() {
 
   const handleSave = async () => {
     try {
-      const endpoint = activeTab === 0 ? `users/${editData.id}` : `events/${editData.id}`;
+      let endpoint;
+      switch (activeTab) {
+        case 0:
+          endpoint = `users/${editData.id}`;
+          break;
+        case 1:
+          endpoint = `events/update/${editData.id}`;
+          break;
+        case 2:
+          endpoint = `donations/${editData.id}`;
+          break;
+        case 3:
+          endpoint = `reservations/${editData.id}`;
+          break;
+        default:
+          throw new Error('Invalid tab');
+      }
       const response = await fetch(`${backendRoute}/${endpoint}`, {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` },
         body: JSON.stringify(editData)
       });
       if (response.ok) {
-        activeTab === 0 ? fetchUsers() : fetchEvents();
+        switch (activeTab) {
+          case 0:
+            fetchUsers();
+            break;
+          case 1:
+            fetchEvents();
+            break;
+          case 2:
+            fetchDonations();
+            break;
+          case 3:
+            fetchReservations();
+            break;
+        }
         setSnackbarOpen(true);
       }
     } catch (error) {
@@ -141,13 +212,42 @@ export default function AdminLanding() {
 
   const handleDelete = async (id) => {
     try {
-      const endpoint = activeTab === 0 ? `users/${id}` : `events/${id}`;
+      let endpoint;
+      switch (activeTab) {
+        case 0:
+          endpoint = `users/${id}`;
+          break;
+        case 1:
+          endpoint = `events/${id}`;
+          break;
+        case 2:
+          endpoint = `donations/${id}`;
+          break;
+        case 3:
+          endpoint = `reservations/${id}`;
+          break;
+        default:
+          throw new Error('Invalid tab');
+      }
       const response = await fetch(`${backendRoute}/${endpoint}`, {
         method: 'DELETE',
         headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` },
       });
       if (response.ok) {
-        activeTab === 0 ? fetchUsers() : activeTab === 1 ? fetchEvents() : fetchReviews();
+        switch (activeTab) {
+          case 0:
+            fetchUsers();
+            break;
+          case 1:
+            fetchEvents();
+            break;
+          case 2:
+            fetchDonations();
+            break;
+          case 3:
+            fetchReservations();
+            break;
+        }
       } else {
         console.error('Failed to delete item');
       }
@@ -160,6 +260,136 @@ export default function AdminLanding() {
     setActiveTab(newValue);
   };
 
+  const renderModalContent = () => {
+    switch (activeTab) {
+      case 0:
+        return (
+          <>
+            <TextField
+              margin="normal"
+              fullWidth
+              id="name"
+              label="Name"
+              name="name"
+              value={editData.name || ''}
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="normal"
+              fullWidth
+              id="email"
+              label="Email"
+              name="email"
+              value={editData.email || ''}
+              onChange={handleInputChange}
+            />
+          </>
+        );
+      case 1:
+        return (
+          <>
+            <TextField
+              margin="normal"
+              fullWidth
+              id="name"
+              label="Name"
+              name="name"
+              value={editData.name || ''}
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="normal"
+              fullWidth
+              id="date"
+              label="Date"
+              name="date"
+              type="date"
+              value={editData.date || ''}
+              onChange={handleInputChange}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+            <TextField
+              margin="normal"
+              fullWidth
+              id="location"
+              label="Location"
+              name="location"
+              value={editData.location || ''}
+              onChange={handleInputChange}
+            />
+          </>
+        );
+      case 2:
+        return (
+          <>
+            <TextField
+              margin="normal"
+              fullWidth
+              id="category"
+              label="Category"
+              name="category"
+              value={editData.category || ''}
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="normal"
+              fullWidth
+              id="location"
+              label="Location"
+              name="location"
+              value={editData.location || ''}
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="normal"
+              fullWidth
+              id="remarks"
+              label="Remarks"
+              name="remarks"
+              value={editData.remarks || ''}
+              onChange={handleInputChange}
+            />
+          </>
+        );
+      case 3:
+        return (
+          <>
+            <TextField
+              margin="normal"
+              fullWidth
+              id="userId"
+              label="User ID"
+              name="userId"
+              value={editData.userId || ''}
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="normal"
+              fullWidth
+              id="eventId"
+              label="Event ID"
+              name="eventId"
+              value={editData.eventId || ''}
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="normal"
+              fullWidth
+              id="status"
+              label="Status"
+              name="status"
+              value={editData.status || ''}
+              onChange={handleInputChange}
+            />
+          </>
+        );
+      default:
+        return null;
+    }
+  };
+
   return (
     <>
       <AdminNavbar />
@@ -168,26 +398,39 @@ export default function AdminLanding() {
         <Tabs value={activeTab} onChange={handleTabChange} centered>
           <Tab label="Users" />
           <Tab label="Events" />
-          <Tab label="Reviews" />
+          <Tab label="Donations" />
+          <Tab label="Reservations" />
         </Tabs>
       </Box>
-      {activeTab === 0 ? (
+      {activeTab === 0 && (
         <DataTable
           columns={userColumns}
           rows={users}
           onEdit={handleOpenModal}
           onDelete={handleDelete}
         />
-      ) : activeTab === 1 ? <DataTable
+      )}
+      {activeTab === 1 && (
+        <DataTable
           columns={eventColumns}
           rows={events}
           onEdit={handleOpenModal}
           onDelete={handleDelete}
         />
-      : (
+      )}
+      {activeTab === 2 && (
         <DataTable
-          columns={reviewColumns}
-          rows={reviews}
+          columns={donationColumns}
+          rows={donations}
+          onEdit={handleOpenModal}
+          onDelete={handleDelete}
+          nestedConfig={donationNestedConfig}
+        />
+      )}
+      {activeTab === 3 && (
+        <DataTable
+          columns={reservationColumns}
+          rows={reservations}
           onEdit={handleOpenModal}
           onDelete={handleDelete}
         />
@@ -210,64 +453,9 @@ export default function AdminLanding() {
           p: 4,
         }}>
           <Typography id="modal-modal-title" variant="h6" component="h2">
-            Edit {activeTab === 0 ? 'User' : 'Event'}
+            Edit {activeTab === 0 ? 'User' : activeTab === 1 ? 'Event' : activeTab === 2 ? 'Donation' : 'Reservation'}
           </Typography>
-          {activeTab === 0 ? (
-            <>
-              <TextField
-                margin="normal"
-                fullWidth
-                id="name"
-                label="Name"
-                name="name"
-                value={editData.name}
-                onChange={handleInputChange}
-              />
-              <TextField
-                margin="normal"
-                fullWidth
-                id="email"
-                label="Email"
-                name="email"
-                value={editData.email}
-                onChange={handleInputChange}
-              />
-            </>
-          ) : (
-            <>
-              <TextField
-                margin="normal"
-                fullWidth
-                id="name"
-                label="Name"
-                name="name"
-                value={editData.name}
-                onChange={handleInputChange}
-              />
-              <TextField
-                margin="normal"
-                fullWidth
-                id="date"
-                label="Date"
-                name="date"
-                type="date"
-                value={editData.date}
-                onChange={handleInputChange}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-              />
-              <TextField
-                margin="normal"
-                fullWidth
-                id="location"
-                label="Location"
-                name="location"
-                value={editData.location}
-                onChange={handleInputChange}
-              />
-            </>
-          )}
+          {renderModalContent()}
           <Button onClick={handleSave} variant="contained" sx={{ mt: 2 }}>
             Save
           </Button>
