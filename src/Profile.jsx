0203e8821cae6de74579b0598a6ba00094dffad8
John <you@example.com>
2024-08-02 03:59:53 +0800

    DONT TOUCH MA REVIEWS

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -404,7 +404,7 @@ export default function Profile() {
 
     return (
         <>
-            {currentUserRole === 'donator' ? <DonatorNavbar /> : <UserNavbar />}
+            {userRole === 'donator' ? <DonatorNavbar /> : <UserNavbar />}
             <Container maxWidth="md">
                 <Paper elevation={3} style={{ padding: '20px', marginTop: '20px' }}>
                     <Typography variant="h4" gutterBottom>
