ad27c8dc78d66f46f14bc2b8c21d00d3947e8b3f
hrtowii <68852354+hrtowii@users.noreply.github.com>
2024-08-12 22:15:31 +0800

    feat: password validation with zod. completed for signup and reset password. added navbar for reset password

--- src/admin/AdminLanding.tsx
+++ src/admin/AdminLanding.tsx
@@ -18,24 +18,6 @@ import { AdminNavbar } from '../components/Navbar';
 import { TokenContext } from '../utils/TokenContext';
 import { backendRoute } from '../utils/BackendUrl';
 import DataTable from '../components/DataTable.tsx';
-import { z } from "zod";
-import validator from "validator";
-export const passwordSchema = z
-  .string()
-  .min(8, "Password must be at least 8 characters long")
-  .refine(
-    (val) => /[A-Z]/.test(val),
-    "Password must contain at least one uppercase letter",
-  )
-  .refine(
-    (val) => /[a-z]/.test(val),
-    "Password must contain at least one lowercase letter",
-  )
-  .refine((val) => /\d/.test(val), "Password must contain at least one number")
-  .refine(
-    (val) => /[!@#$%^&*(),.?":{}|<>]/.test(val),
-    "Password must contain at least one special character",
-  );
 
 const getUserRole = (user) => {
   if (user.admin) return 'Admin';
